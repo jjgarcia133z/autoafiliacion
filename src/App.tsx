12283822a031eba075e520c1cc92840a56cd9adc
Miguel Esquivel <mesquivelc@medismart.net>
2023-03-01 16:05:36 -0600

    Se agrego bootstrap y sass y layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,52 +1,11 @@
-import React, { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
-import { useSelector } from 'react-redux/es/exports'
-import { RootState } from './stores/store'
-
-interface User {
-  nombre: string,
-  telefono: string,
-  edad: number,
-}
-
+import React from 'react'
+import { BrowserRouter, Router, Route } from 'react-router-dom'
+import 'bootstrap/dist/css/bootstrap-grid.css'
+import './index.css'
 function App() {
-	const [count, setCount] = useState(0)
-	const userState = useSelector((state: RootState) => state.autoAfiliacion)
-	const { user } = userState
-
-	const handleCount = ()=>{
-		setCount((a) => a + 2)
-	}
-
-	const usuario: User = {
-		nombre: 'Juan',
-		telefono: '123456789',
-		edad: 20,
-	}
-
 	return (
-		<div className="App">
-			<div>
-				<a href="https://vitejs.dev" target="_blank" rel="noreferrer">
-					<img src="/vite.svg" className="logo" alt="Vite logo" />
-				</a>
-				<a href="https://reactjs.org" target="_blank" rel="noreferrer">
-					<img src={reactLogo} className="logo react" alt="React logo" />
-				</a>
-			</div>
-			<h1>Vite + {user.nombre}</h1>
-			<div className="card">
-				<button onClick={handleCount}>
-          count is {count}
-				</button>
-				<p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-				</p>
-			</div>
-			<p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-			</p>
+		<div className="container">
+			
 		</div>
 	)
 }
---
+++ src/layout/mainLayout.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const mainLayout = () => {
+
+	return (
+		<main>
+			<header></header>
+			<section>
+				<aside></aside>
+				<section></section>
+			</section>
+			<footer></footer>
+		</main>
+	)
+}
+
+export default mainLayout
