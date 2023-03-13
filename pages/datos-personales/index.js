4b3fadceba64110f304e382244c84e8fc6b5596d
Miguel Esquivel <mesquivelc@medismart.net>
2023-03-13 12:40:35 -0600

    Se agrega pagina extra para datos personales

---
+++ pages/datos-personales/index.js
@@ -0,0 +1,12 @@
+import Layout from '@/components/layouts/Layout'
+import CMP09 from '@/components/contenedores/CMP09'
+
+export default function TipoPlan() {
+	return (
+		<>
+			<Layout title="Tipo Plan url - Autoafiliacion">
+				<CMP09 text="test" />
+			</Layout>
+		</>
+	)
+}
