f6641b53d9a8a8b2f8d14966c4b9f067b660db57
Miguel Esquivel <mesquivelc@medismart.net>
2023-03-13 09:07:20 -0600

    Contenedor tipo plan

--- components/contenedores/CMP09.jsx
+++ components/contenedores/CMP09.jsx
@@ -3,17 +3,49 @@ import styled from 'styled-components'
 
 const CMP09 = () => {
 	return (
-		<Container>CMP09</Container>
+		<Container>
+			<div>
+				<h1>Plan Medismart</h1>
+				<p>
+          Adquirí el mejor plan de medicina prepagada que te aporta beneficios
+          en citas y servicios médicos para vos, tus amigos y familiares.
+				</p>
+			</div>
+			<div></div>
+		</Container>
 	)
 }
 
 export default CMP09
 
 const Container = styled.div`
+  width: 100%;
+  height: 100%;
+  /* background-color: #ff000045; */
+  padding: 32px;
+  & > div {
     width: 100%;
-    height: 100%;
-    background-color: #ff000045;
-    padding: 32px;
-
-
+    margin-top: calc(227px - 32px);
+	
+    & > h1 {
+      font-family: "Radley";
+      font-size: 42px;
+      font-weight: 400;
+      line-height: 50.4px;
+      letter-spacing: 0px;
+      text-align: left;
+	  text-transform: uppercase;
+	  color: var(--primary-blue-primary-blue-900)
+    }
+    & > p {
+      font-family: Montserrat;
+      font-size: 16px;
+      font-weight: 500;
+      line-height: 19.2px;
+      letter-spacing: 0px;
+      text-align: left;
+	  max-width: 696px;
+	  color: var(--primary-blue-primary-blue-900)
+    }
+  }
 `
