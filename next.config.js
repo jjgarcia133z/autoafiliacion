af8972e47952d1f09dab33561a4353c1495c18b2
Miguel Esquivel <mesquivelc@medismart.net>
2023-03-14 11:23:19 -0600

    Configuracion y packete nuevo

--- next.config.js
+++ next.config.js
@@ -1,10 +1,22 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-  compiler: {
-    // Enables the styled-components SWC transform
-    styledComponents: true
-  }
+	reactStrictMode: true,
+	compiler: {
+		// Enables the styled-components SWC transform
+		styledComponents: true,
+
+		esModuleInterop: true,
+		webpack: (config) => {
+			config.module.rules.push({
+				test: /\.m?js$/,
+				resolve: {
+					fullySpecified: false,
+				},
+			})
+
+			return config
+		},
+	},
 }
 
 module.exports = nextConfig
