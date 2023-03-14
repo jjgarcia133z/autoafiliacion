c524b1a040866302e41553e8b1f2a442a416b12d
Miguel Esquivel <mesquivelc@medismart.net>
2023-03-14 11:17:59 -0600

    Se ajusto diseño del hover y se formateo el codigo

---
+++ components/common/CMP05.jsx
@@ -0,0 +1,52 @@
+import React from 'react'
+import styled from 'styled-components'
+import { FiChevronLeft as IconLeft } from 'react-icons/fi'
+// import IconLefCaret from '../../assets/img/icons/icon-outlines-28@2x.png'
+const CMP05 = () => {
+	return (
+		<ContainerBtnExit>
+			<IconLeft />
+			<span>Salir</span>
+		</ContainerBtnExit>
+	)
+}
+
+export default CMP05
+
+const ContainerBtnExit = styled.button`
+  display: flex;
+  border: none;
+  background: none;
+  outline: none;
+  flex-direction: row;
+  justify-content: flex-start;
+  gap: 8px;
+  align-items: center;
+  padding: 4px 0px 8px 0px;
+  margin-bottom: 32px;
+  &:hover span, &:hover svg {
+    cursor: pointer;
+    color: var(--secundary-accent-secundary-accent-main-500);
+  }
+
+  & > span {
+    //styleName: Body/Body Small - Montserrat Regular;
+    font-family: Montserrat;
+    font-weight: 400;
+    font-size: 14px;
+    line-height: 17px;
+    letter-spacing: 0px;
+    text-align: left;
+    color: var(--neutral-gray-colors-neutral-white);
+  }
+  & img {
+    width: 24px;
+    height: 24px;
+    color: #a6cb83;
+  }
+  & svg {
+    width: 24px;
+    height: 24px;
+    color: var(--neutral-gray-colors-neutral-white);
+  }
+`
