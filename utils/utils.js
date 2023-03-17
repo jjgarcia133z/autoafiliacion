1be0791c9f78ce2dc63129e9460eb39a097ba8fd
Miguel Esquivel <miguel.gesquivel@gmail.com>
2023-03-17 15:19:27 -0600

    ajuste de las posiciones

--- utils/utils.js
+++ utils/utils.js
@@ -4,80 +4,105 @@ const selectTooltipPosition = (
 	height,
 	width,
 	parentHeight,
-	parentWidth
+	parentWidth,
+	separation = 15,
+	pseudoElement = {
+
+	}
 ) => {
-	switch (tipPosition) {
-	case position.topMid:
-		return `
-                top: ${(height + 15) * -1}px;
-                transform: translateX(calc(50% + ${width / 2 / 2}px));
+	let arrowConerGap = 35
+	//Si la separación es un string, lo convertimos a número
+	if (typeof separation === 'string' && separation.endsWith('px')) {
+		separation = Number(separation.replace('px', ''), 10)
+	} else if (typeof separation === 'string') {
+		separation = Number(separation, 10)
+	}
+
+	tipPosition = tipPosition == position.top ? position.topMid : tipPosition
+	tipPosition =
+    tipPosition == position.bottom ? position.bottomMid : tipPosition
+	tipPosition = tipPosition == position.left ? position.leftMid : tipPosition
+	tipPosition = tipPosition == position.right ? position.rightMid : tipPosition
+	const pseudoElementMargin = pseudoElement.margin ? true : false
+	
+	if (!pseudoElementMargin) {	
+
+		switch (tipPosition) {
+		case position.topMid:
+			return `
+                top: ${(height + separation) * -1}px;
+                left: ${parentWidth / 2 - width / 2}px;
+                
             `
-	case position.topRight:
-		return `
-                top: ${(height + 15) * -1}px;
-                right: -${width / 2 / 0.65}px;
+		case position.topRight:
+			return `
+                top: ${(height + separation) * -1}px;
+                right: -${width - arrowConerGap}px;
             `
-	case position.topLeft:
-		return `
-                top: ${(height + 15) * -1}px;
-                left: -${width / 2 / 0.6}px;
+		case position.topLeft:
+			return `
+                top: ${(height + separation) * -1}px;
+                left: -${width - arrowConerGap}px;
             `
-	case position.bottomMid:
-		return `
-                bottom: ${(height + 15) * -1}px;
-                transform: translateX(calc(50% + ${width / 2 / 2}px));
+		case position.bottomMid:
+			return `
+                bottom: ${(height + separation) * -1}px;
+                left: ${parentWidth / 2 - width / 2}px;
             `
-	case position.bottomRight:
-		return `
-                bottom: ${(height + 15) * -1}px;
-                right: -${width / 2 / 0.65}px;
+		case position.bottomRight:
+			return `
+                bottom: ${(height + separation) * -1}px;
+                right: -${width - arrowConerGap}px;
             `
-	case position.bottomLeft:
-		return `
-                bottom: ${(height + 15) * -1}px;
-                left: -${width / 2 / 0.5}px;
+		case position.bottomLeft:
+			return `
+                bottom: ${(height + separation) * -1}px;
+                left: -${width - arrowConerGap}px;
             `
-	case position.leftMid:
-		return `
-                left: ${(width + 15) * -1}px;
-                top: -${(height / 2) * 0.7}px; 
+		case position.leftMid:
+			return `
+                left: ${(width + separation) * -1}px;
+                top:-${height / 2 - parentHeight / 2}px;
             `
-	case position.leftTop:
-		return `
-                left: ${(width + 15) * -1}px;
-                top:  -${height - 20}px;
+		case position.leftTop:
+			return `
+                left: ${(width + separation) * -1}px;
+                top: -${height - arrowConerGap}px;
             `
-	case position.leftBottom:
-		return `
-                left: ${(width + 15) * -1}px;
-                bottom:  -${height - 20}px;
+		case position.leftBottom:
+			return `
+                left: ${(width + separation) * -1}px;
+                bottom: -${height - arrowConerGap}px;
             `
-	case position.rightMid:
-		return `
-                right: ${(width + 15) * -1}px;
-                top:-${(height / 2) - (parentHeight / 2)}px;
+		case position.rightMid:
+			return `
+                right: ${(width + separation) * -1}px;
+                top:-${height / 2 - parentHeight / 2}px;
             `
-	case position.rightTop:
-		return `
-                right: ${(width + 15) * -1}px;
-                top:  -${height - 20}px;
+		case position.rightTop:
+			return `
+                right: ${(width + separation) * -1}px;
+                top: -${height - arrowConerGap}px;
             `
-	case position.rightBottom:
-		return `
-                right: ${(width + 15) * -1}px;
-                bottom:  -${height - 20}px;
+		case position.rightBottom:
+			return `
+                right: ${(width + separation) * -1}px;
+                bottom: -${height - arrowConerGap}px;
             `
-	case position.center:
-		return `
-                top: 0;
-                left: 0;
+		case position.center:
+			return `
+                top: 50%;
+                left: 50%;
                 transform: translate(-50%, -50%);
             `
-	default:
-		return `
+		default:
+			return `
                 top: 0;
                 left: 0;
             `
+		}
+	} else {		
+		arrowConerGap = 0
 	}
 }
 
